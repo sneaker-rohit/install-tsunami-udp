edcdfc15cb34c6a6a42ece001e7a5ec1d2f8bfda
rohittahiliani <rohit.tahil@gmail.com>
2016-10-17 03:34:04 +0530

    shell script to install tsunami udp

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # install-tsunami-udp
 Contains the shell script which installs the necessary packages and the code required to run tsunami-udp. For more information on tsunami-udp please visit tsunami-udp.sourceforge.net
+
+
+To perform the file transfers 
+
+tsunamid --port 46224 * # (Serves all files from current directory for copy)
+
+tsunami connect [server] get * # (copies all files served to current directory)
---
+++ install-tsunami-udp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Shell script to install tsunami-udp protocol
+# Ensure that you are running the script as a superuser 
+
+apt-get -y install make gcc autoconf
+# Download the sourcecode of tsunami-udp 
+wget http://downloads.sourceforge.net/project/tsunami-udp/tsunami-udp/tsunami-v1.1-cvsbuild42/tsunami-v1.1-cvsbuild42.tar.gz
+# Extract the code 
+tar zxvf tsunami-v1.1-cvsbuild42.tar.gz
+cd tsunami-udp-v11-b42
+aclocal
+autoconf
+# Install the missing pack
+automake --add-missing
+./configure
+make
+./recompile.sh 
+# install the additional binaries
+sudo make install
